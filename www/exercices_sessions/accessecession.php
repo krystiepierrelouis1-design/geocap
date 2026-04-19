dcc3a5c386f2cf70fb84ccd7a4d202f17bbbe921
kryslenie94 <krystiepierrelouis1@gmail.com>
2026-04-19 11:11:54 +0200

    Phase 3 - Début développement GEOCAP

--- www/exercices_sessions/accessecession.php
+++
@@ -1,9 +0,0 @@
-<?php
-// access_session .php
-session_start ();
-if (isset( $_SESSION ["nom"])) {
-echo " Bienvenue " . $_SESSION ["nom"];
-} else {
-echo "Aucune session utilisateur active .";
-}
-?>
