e9a4090936c0ea071e2ab8be8c866cf16893ef2a
kryslenie94 <krystiepierrelouis1@gmail.com>
2026-04-23 13:06:06 +0200

    Correction chemin CSS et logo header

--- www/includes/header.php
+++ www/includes/header.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GEOCAP</title>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
 <header>
     <nav class="navbar">
         <div class="logo">
-            <img src="assets/img/logo.png" alt="Logo GEOCAP" height="50">
+            <img src="../assets/img/logo.png" alt="Logo GEOCAP" height="50">
             <span>GEOCAP</span>
         </div>
         <ul class="menu">
